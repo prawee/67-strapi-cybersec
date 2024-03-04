495c5eb5e277b6226e44f0094eff7571e35201f1
Prawee Wongsa <prawee@hotmail.com>
2024-03-04 20:59:38 +0700

    how to encode data

---
+++ src/api/student/content-types/student/lifecycles.js
@@ -0,0 +1,8 @@
+const md5 = require('md5')
+module.exports = {
+    async beforeCreate(event) {
+        console.log('beforeCreate is work!', event.params.data)
+        // event.params.data.mobile = 'yxz'
+        event.params.data.mobile = md5(event.params.data.mobile)
+    }
+}
